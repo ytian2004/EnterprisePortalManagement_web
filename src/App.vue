6a88dcc40f8f90eacf870d810741ac23a471efb9
orange-88 <2835973787@qq.com>
2025-02-22 16:57:19 +0800

    modified src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <NavBar/>
-  <router-view/>
+  <div>
+    <NavBar />
+    <router-view />
+  </div>
 </template>
 <script setup>
-import NavBar from '@/components/NavBar.vue'
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <style lang="scss">
- *{
+* {
   margin: 0;
   padding: 0;
- }
-::-webkit-scrollbar{
+}
+::-webkit-scrollbar {
   width: 5px;
   height: 5px;
-  position: absolute
+  position: absolute;
 }
 
-::-webkit-scrollbar-thumb{
+::-webkit-scrollbar-thumb {
   background-color: #1890ff;
 }
 
-::-webkit-scrollbar-track{
+::-webkit-scrollbar-track {
   background-color: #ddd;
 }
 </style>
